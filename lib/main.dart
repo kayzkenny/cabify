4f517a0d57820705511f01ce402c3e1fa2e57765
Kehinde Akeredolu <9991881+kayzkenny@users.noreply.github.com>
2021-01-16 00:49:57 +0100

    setup firebase project for ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,9 +24,30 @@ class App extends StatelessWidget {
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // Create a CollectionReference called users that references the firestore collection
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    Future<void> addUser() {
+      // Call the user's CollectionReference to add a new user
+      return users
+          .add({
+        'full_name': "fullName", // John Doe
+        'company': "company", // Stokes and Sons
+        'age': "age" // 42
+      })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+
     return Scaffold(
       body: Center(
-        child: Text('Landing Page'),
+        child: FlatButton(
+          onPressed: addUser,
+          child: Text(
+            "Add User",
+          ),
+        ),
       ),
     );
   }
