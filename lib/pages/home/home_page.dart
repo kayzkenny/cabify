3fa7f199a5a0be3988065d5486b44a733707bdf4
Kehinde Akeredolu <9991881+kayzkenny@users.noreply.github.com>
2021-01-19 14:11:19 +0100

    build: show map on home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,33 +1,86 @@
+import 'dart:async';
+
+import 'package:cabify/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:cabify/providers/auth_provider.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:cabify/providers/auth_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    Future<void> signOut() async {
-      await context.read(authServiceProvider).signOut();
-      // Navigator.pushReplacementNamed(context, '/');
-    }
+    final searchBarTop = useState(0.0);
+    Completer<GoogleMapController> _controller = Completer();
+
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
+    final CameraPosition _kLake = CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(37.43296265331129, -122.08832357078792),
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414);
+
+    // Future<void> signOut() async {
+    //   await context.read(authServiceProvider).signOut();
+    //   // Navigator.pushReplacementNamed(context, '/');
+    // }
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Cabify",
-          style: TextStyle(color: Colors.black, fontSize: 32.0),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-      ),
-      body: SafeArea(
-        child: Center(
-          child: FlatButton(
-            onPressed: signOut,
-            child: Text('Log Out'),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              searchBarTop.value = 64.0;
+            },
           ),
-        ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.fastOutSlowIn,
+            top: searchBarTop.value,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              height: 48.0,
+              width: 360.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 5.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  // labelText: 'Where to?',
+                  hintText: 'Where to?',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
